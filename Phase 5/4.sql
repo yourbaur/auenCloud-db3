1ba782b466317e63a118c41df410ad6d23165ca7
KanatSeytov <55532407+KanatSeytov@users.noreply.github.com>
2021-04-05 08:09:27 +0600

    Update 4.sql

--- Phase 5/4.sql
+++ Phase 5/4.sql
@@ -1,5 +1,6 @@
-insert into top_music 
-select id, popularity 
-from music 
-order by popularity
-limit 10;
+select to_char(m.id) as id, to_char(year) as year, name, artist, to_char(popularity) popularity, 
+(CASE WHEN tp.id is null THEN 0 ELSE 1 END) as added 
+from music m 
+left join top_music tp 
+on m.id=tp.id 
+order by m.id desc
