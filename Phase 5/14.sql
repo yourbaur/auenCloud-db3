da5455873fa53f158ea2f53dfd3721462e59e9d8
KanatSeytov <55532407+KanatSeytov@users.noreply.github.com>
2021-04-18 23:37:36 +0600

    Update 14.sql

--- Phase 5/14.sql
+++ Phase 5/14.sql
@@ -1,1 +1,1 @@
-select m1.name from music m1 where m1.year = (select m2.album from music m2 where m2.name = '5000'  and m2.artist='IK')
+select m1.name from music m1 where m1.year = (select m2.year from music m2 where m2.name = '5000'  and m2.artist='IK')
