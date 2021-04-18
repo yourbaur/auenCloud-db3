2daa0b59716aabc52b71b500b0cfaf07640d3e34
KanatSeytov <55532407+KanatSeytov@users.noreply.github.com>
2021-04-18 23:38:54 +0600

    Update 14.sql

--- Phase 5/14.sql
+++ Phase 5/14.sql
@@ -1,1 +1,1 @@
-select m1.name from music m1 where m1.year = (select m2.year from music m2 where m2.name = '5000'  and m2.artist='IK')
+select m1.name from music m1 where m1.year in (select m2.year from music m2 where m2.name = 'Oli mera paizei zaria'  and m2.artist='Giorgos Katsaros')
